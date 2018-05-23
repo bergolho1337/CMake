3aab293b117a9ba1cf5791143e209e0481954d9d
Lucas <berg@ice.ufjf.br>
2018-05-23 08:42:50 -0300

    Use an array instead of single variables

--- FunctionPointer/Version-2/Main-Program/main.c
+++ FunctionPointer/Version-2/Main-Program/main.c
@@ -58,33 +58,46 @@ int main ()
     // Handle to the DL library file
     // Pass the path to shared_libs folder
     void *handle;
+    sample_function_fn **func_array = (sample_function_fn**)malloc(sizeof(sample_function_fn*)*3);
     handle = open_shared_library("../Shared-Library/shared_libs/libfunctions1.so");
 
-    // Use the same function pointer as defined on the 'model_common' header file
-    sample_function_fn *func1 = dlsym(handle, "sample_function");
+    func_array[0] = dlsym(handle, "sample_function");
 
     handle = open_shared_library("../Shared-Library/shared_libs/libfunctions2.so");
-    sample_function_fn *func2 = dlsym(handle, "sample_function");
+    func_array[1] = dlsym(handle, "sample_function");
 
     handle = open_shared_library("../Shared-Library/shared_libs/libfunctions3.so");
-    sample_function_fn *func3 = dlsym(handle, "sample_function");
+    func_array[2] = dlsym(handle, "sample_function");
+
+    // Use the same function pointer as defined on the 'model_common' header file
+    //sample_function_fn *func1 = dlsym(handle, "sample_function");
+
+    //handle = open_shared_library("../Shared-Library/shared_libs/libfunctions2.so");
+    //sample_function_fn *func2 = dlsym(handle, "sample_function");
+
+    //handle = open_shared_library("../Shared-Library/shared_libs/libfunctions3.so");
+    //sample_function_fn *func3 = dlsym(handle, "sample_function");
 
     // Call the function through the function pointer
     fprintf(stdout,"[!] Calling function 1 (double)\n");
-    func1(v1,N);
+    //func1(v1,N);
+    func_array[0](v1,N);
     printVector(v1,N);
 
     fprintf(stdout,"[!] Calling function 2 (triple)\n");
-    func2(v2,N);
+    //func2(v2,N);
+    func_array[1](v2,N);
     printVector(v2,N);
 
     fprintf(stdout,"[!] Calling function 3 (quadruple)\n");
-    func3(v3,N); 
+    //func3(v3,N); 
+    func_array[2](v3,N);
     printVector(v3,N);
 
     free(v1);
     free(v2);
     free(v3);
+    free(func_array);
 
     return 0;
 }
